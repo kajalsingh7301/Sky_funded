4c313627c21c267ae174feabe87cab0d3d7b437a
python coding <clcoding.py@gmail.com>
2025-09-19 16:23:30 +0530

    new build push

--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const Deposit = require("../models/Deposit");
-const KYC = require("../models/KYC");
+const KYC = require("../models/Kyc");
 let Ticket;
 
 try {
